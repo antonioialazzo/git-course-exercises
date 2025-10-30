18ca76210585e353a02ba85cfa9b0e5771480b26
antonioialazzo <endoni@DESKTOP-T7JEQF4.localdomain>
2025-10-30 18:36:46 +0100

    Solution to binary_converter.cpp

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,26 @@
   Insert first number: 8
   The binary number is: 1000
 */
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string binary_converter(int n){
+  string binary = "";
+  while (n>0) {
+    if(n%2) binary.append(1, '1');
+    else binary.append(1, '0');
+    n=n/2;
+  }
+  reverse(binary.begin(), binary.end());
+  return binary;
+}
+
+int main () {
+  int number;
+  cout << "Insert first number: " << endl;
+  cin >> number;
+  cout << "The binary number is: " << binary_converter(number) <<endl;
+
+}
