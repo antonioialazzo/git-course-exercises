57c61208163bf390b9bf3e60d5ae2d5bdaac967a
antonioialazzo <endoni@DESKTOP-T7JEQF4.localdomain>
2025-11-10 18:32:10 +0100

    feat: solution to random_number.cpp and risk-risiko.cpp

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,13 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(nullptr));
+    int x = rand() % 50; //scelgo un range da 0 a 49
+    cout << "The random number is: " << x << endl;
+}
